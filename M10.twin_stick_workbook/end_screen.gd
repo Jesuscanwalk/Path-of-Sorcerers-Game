870c8a741d2e280b00c2a812e7198bb1a5acbfe1
Jesuscanwalk <159198277+Jesuscanwalk@users.noreply.github.com>
2025-02-12 13:33:16 -0800

    buttons work work work work work

--- M10.twin_stick_workbook/end_screen.gd
+++ M10.twin_stick_workbook/end_screen.gd
@@ -6,14 +6,6 @@ var start_time := Time.get_ticks_msec()
 @onready var quit_button: Button = %QuitButton
 
 
-func ready() -> void:
-	visible = false
-	retry_button.pressed.connect(func ()-> void:
-		get_tree().paused = false
-		get_tree().reload_current_scene()
-	)
-	quit_button.pressed.connect(get_tree().quit)
-
 
 func open() -> void:
 	visible = true
@@ -22,3 +14,11 @@ func open() -> void:
 	var total_time_msec := end_time - start_time
 	var total_time_s := snappedf(total_time_msec / 1000.0, 0.1)
 	time_label.text = "Time: " + str(total_time_s) + "s"
+
+
+func _on_retry_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit() 
